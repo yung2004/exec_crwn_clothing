f5a9cbe9db5948601fd473de9d17e3ad82f27035
eric yung <yung2004@gmail.com>
2020-08-21 23:28:20 +0800

    lesson 6 completed

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
  import React from 'react';
 
  import FormInput from '../form-input/form-input.component';
- 
+  import CustomButton from '../custom-button/custom-button.component';
  import './sign-in.styles.scss';
 
  class SignIn extends React.Component {
@@ -42,7 +42,7 @@
                         label="Password"
                         required/>
 
-                    <input type="submit" value="Submit Form" />
+                    <CustomButton type="submit" value="Submit Form" > Sign in                        </CustomButton>
                 </form>
             </div>
         );
