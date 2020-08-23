53e0cbc332b4c8347981a7112a2e2d570c476ca0
eric yung <yung2004@gmail.com>
2020-08-23 23:08:24 +0800

    finishing sign-in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,8 +3,8 @@
  import FormInput from '../form-input/form-input.component';
  import CustomButton from '../custom-button/custom-button.component';
  
- import { signInWithGoogle } from '../../firebase/firebase.utils'
-
+ import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
+ 
  import './sign-in.styles.scss';
 
 
@@ -19,9 +19,18 @@
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState( {email: '', password: ''});
+
+        const { email, password } = this.state;
+    
+        try {
+          await auth.signInWithEmailAndPassword(email, password);
+          
+          this.setState({ email: '', password: '' });
+        } catch (error) {
+          console.log(error);
+        }
     }
 
     handleChange = event => {
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,41 +12,38 @@ const config = {
     appId: "1:463225307429:web:045dfa3ed2e883d3b05acc"
 };
 
-export const createUserProfileDocument = async(userAuth, additionalData) => {
-    if (!userAuth) return;
-    
-    const userRef= firestore.doc(`users/${userAuth.uid}`);
-    // .get is async call 
-    const snapshot = await userRef.get();
-
-    if (!snapshot.exists) {
-        const { displayName, email} = userAuth;
-        const createdAt = new Date();
-
-        try {
-            await userRef.set( {
-                displayName,
-                email,
-                createdAt,
-                ...additionalData 
-            });
-        } catch (error) {   
-            console.log('error', error.message)
-
-        }
-    }
-    console.log(snapshot);
-
-    return userRef;
-};
-
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const provide = new firebase.auth.GoogleAuthProvider();
-provide.setCustomParameters( { prompt: 'select_account' });  //pop up 
-export const signInWithGoogle = () => auth.signInWithPopup(provide);
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
