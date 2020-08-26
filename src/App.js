5c7fef4eab9b95d51f2bf614988d50509f0ccb06
eric yung <yung2004@gmail.com>
2020-08-26 23:36:44 +0800

    110

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import {connect } from 'react-redux';
 
 import './App.css';
@@ -69,7 +69,12 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin'
+            render={ () =>
+              this.props.currentUser ? 
+              (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)
+            }
+          />
   
         </Switch>
       </div>
@@ -77,8 +82,12 @@ class App extends React.Component {
   }  
 }
 
+
+const mapStateToProps = ( {user}) => ({
+  currentUser: user.currentUser
+})
 const mapDispatchToRops = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToRops)(App);
+export default connect(mapStateToProps, mapDispatchToRops)(App);
