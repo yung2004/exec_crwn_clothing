d1a8b7c160b8b2a3313e401fbd3d4f4b0d84f8e4
eric yung <yung2004@gmail.com>
2020-10-05 23:36:39 +0800

    create respective selectors and updated corresponding components for directory/shop

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -7,10 +7,18 @@ import "./collection.styles.scss";
 
 const CollectionPage = ({ collection }) => {
   console.log('ColectionPage ',  collection);
+
+  const {title, items } = collection;
   return (
-    <div className="category">
-      <h2>collection PAGE</h2>
-    </div>
+    <div className="collection-page">
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {
+          items.map(item=> <CollectionItem key={item.id}
+            item={item} />) 
+        }
+      </div>
+    </div> 
   );
 };
 
--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -19,14 +19,14 @@ const INITIAL_STATE = {
           linkUrl: 'shop/sneakers'
         },
         {
-          title: 'sharon',
+          title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
           id: 4,
           linkUrl: 'shop/womens'
         },
         {
-          title: 'andy',
+          title: 'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
           id: 5,
